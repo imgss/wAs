46b7892a32bfd38e4f190fd24e73654538d17194
imgss <838632526@qq.com>
2019-08-26 10:24:02 +0800

    English

--- alert.js
+++ alert.js
@@ -4,10 +4,9 @@ export function failAlert() {
   window.gameOver = true;
   let stuckedWolves = wolf.wolves.filter(w => w.stuck > 0);
   swal({
-    text: `你围住了${stuckedWolves.length}只狼!`,
-    buttons: ['再来一次']
+    text: `you killed ${stuckedWolves.length} wolves!`,
+    buttons: ['REPLAY']
   }).then(v => {
-    console.log(v);
     if (v) {
       window.location.reload();
     }
